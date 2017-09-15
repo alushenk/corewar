28cec530f3666ec4f2d0873a543a268a09377d6b
alushenk <alushenk@e3r7p6.unit.ua>
2017-09-15 21:42:20 +0300

    play/pause and steps are ready. need to write carriages into objects during parsing

--- visual/main.py
+++ visual/main.py
@@ -2,92 +2,12 @@
 
 from __future__ import print_function
 import sys
-from collections import deque
-import pygame
+
 from parse import parse
-import binascii
-
-# colors
-background_color = (39, 40, 34)
-element_color = (70, 70, 70)
-text_color = (180, 180, 180)
-player_color = (0, 255, 0)
-highlighted_text_color = (0, 0, 0)
-
-# sizes
-space = 1
-element_size = 16
-menu_width = 200
-height = (element_size + space) * 64
-width = height + menu_width
-
-# temporary defines
-player_start_index = 1
-size = 12
-
-# font
-# font_name = "raleway/Raleway-Thin.ttf"
-font_name = "quicksand/Quicksand-Light.ttf"
-font_size = element_size + 2
+from draw import draw_map
 
 
-def draw_map(i, j, index, screen, font, step):
-    x = j * (element_size + space)
-    y = i * (element_size + space)
-    if player_start_index <= index <= player_start_index + size:
-        current_text_color = highlighted_text_color
-        current_player_color = player_color
-    else:
-        current_text_color = text_color
-        current_player_color = element_color
-    pygame.draw.rect(screen, current_player_color, [x, y, element_size, element_size])
-
-    text = font.render(format(step.field[index] & 240, '02x')[0].upper(), True, current_text_color)
-    screen.blit(text, (x + 1, y + 3))
-    text = font.render(format(step.field[index] & 15, '02x')[1].upper(), True, current_text_color)
-    screen.blit(text, (x + 9, y + 3))
-
-    # text = font.render(memory[index].encode('hex').upper(), True, current_text_color)
-    # screen.blit(text, (x + 1, y  + 3))
-
-    # value = format(step.field[index], '02x').upper()
-    # text = font.render(value, True, current_text_color)
-    # screen.blit(text, (x + 1, y + 2))
-
-
-def main(steps):
-    pygame.init()
-
-    screen = pygame.display.set_mode((
-        int(width),
-        int(height)),
-        pygame.RESIZABLE)
-    screen.fill(background_color)
-    font = pygame.font.SysFont(
-        font_name,
-        font_size,
-        bold=False,
-        italic=False)
-
-    loop = True
-    while loop:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                loop = False
-            if event.type == pygame.QUIT:
-                loop = False
-            index = 0
-            for i in range(64):
-                for j in range(64):
-                    draw_map(i, j, index, screen, font, steps[0])
-                    index += 1
-        pygame.display.update()
-
-    pygame.quit()
-    quit()
-
-
-if __name__ == "__main__":
+def main():
     if len(sys.argv) == 2:
         with open(sys.argv[1], 'rb') as file:
             data = file.read()
@@ -95,7 +15,11 @@ if __name__ == "__main__":
         data = bytearray(data)
         steps = parse(data)
 
-        main(steps)
+        draw_map(steps)
     else:
         print('usage: ./main.py [output]')
         exit()
+
+
+if __name__ == "__main__":
+    main()

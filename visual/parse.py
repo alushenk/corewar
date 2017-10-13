231175b012140404644f1e062cffd35cc192f855
alushenk <alushenk@e1r8p8.unit.ua>
2017-10-13 20:12:59 +0300

    parse.py -> 84

--- visual/parse.py
+++ visual/parse.py
@@ -7,42 +7,30 @@ from classes import Player, Step, Carriage, bin_to_int
 
 def parse_players(file):
 
-    file.seek(0)
     number_of_players = struct.unpack('B', file.read(1))[0]
     # number_of_players = int.from_bytes(file.read(1), byteorder='big')
     # number_of_players = int(file.read(1))
     # number_of_players = data[index]
-    file.seek(1, 1)
 
     players = []
-    while number_of_players > 0:
+    for i in range(number_of_players):
         player = Player(struct.unpack('B', file.read(1))[0])
         #player = Player(data[index])
-        file.seek(1, 1)
 
         player.pc = file.read(4)
         # player.pc = data[index: index + 4]
-        file.seek(4, 1)
 
         player.set_size(file.read(4))
         # player.set_size(data[index: index + 4])
-        file.seek(4, 1)
 
-        data = bytearray(file.read(128))
         # player.set_name(struct.unpack('s', data)[0])
-        player.set_name(data)
-        print(player.name)
+        player.set_name(file.read(129))
         # player.set_name(data[index: index + 129])
-        file.seek(128, 1)
 
-        data = bytearray(file.read(2048))
-        player.set_comment(data)
-        print(player.comment)
+        player.set_comment(file.read(2049))
         # player.set_comment(data[index: index + 2049])
-        file.seek(2048, 1)
 
         players.append(player)
-        number_of_players -= 1
 
     return players, number_of_players
 
@@ -51,9 +39,9 @@ def parse_step(file, index):
     step = Step()
 
     file.seek(index, 1)
-    step.field = file.read(4096)
+    step.field = file.read(4097)
     # step.field = data[index: index + 4097]
-    index += 4096
+    index += 4097
 
     file.seek(index, 1)
     number_of_carriages = bin_to_int(file.read(4))
@@ -90,14 +78,15 @@ def parse_step(file, index):
 
 
 def parse_step_size(file, index):
-    index += 4096
+    index += 4097
 
     file.seek(index, 1)
     number_of_carriages = bin_to_int(file.read(4))
     # number_of_carriages = bin_to_int(data[index: index + 4])
+    file.seek(-8, 1)
     index += 4
 
-    index += 10 * number_of_carriages
+    index += (number_of_carriages * 10)
 
     return index
 
@@ -107,6 +96,8 @@ def parse(file, file_size):
 
     # steps = deque()
     indexes = list()
+    index = 0
+    indexes.append(index)
     while index < file_size:
         # step, index = parse_step(data, index)
         # steps.append(step)
